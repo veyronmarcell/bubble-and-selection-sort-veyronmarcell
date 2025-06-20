3b271887cb6d4cd716242c1785c020a23826092b
veyronmarcell <mark.kiplangat@strathmore.edu>
2025-06-20 12:55:19 +0300

    adding selection and bubble

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,13 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    pass
+    
+    n=len(unsorted_list)
+    for i in range(n-1):
+     for j in range (n-i-1):
+        if unsorted_list[j]>unsorted_list[j+1]:
+           unsorted_list[j],unsorted_list[j+1]=unsorted_list[j+1],unsorted_list[j]
+    
+    return unsorted_list
+
+
+print(bubble_sort([3,2,1]))
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,15 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+
+    n=len(arr)
+    for i in range(n-1):
+        min_index=i
+        for j in range(i+1,n):
+            if arr[j]<arr[min_index]:
+                min_index=j
+        arr[i],arr[min_index]=arr[min_index],arr[i]
+    
+    return arr
+
+print(selection_sort([1, 2, 3]))  # Should return [1, 2, 3]
+print(selection_sort([3, 2, 1]))  # Should return [1, 2, 3]
+print(selection_sort([29, 10, 14, 37, 14]))  # Should return [10, 14, 14, 29, 37]
